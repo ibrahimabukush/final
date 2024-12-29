9eb1e2d2c405b03d5e6117ad792386f6f921b2eb
A7madEl <ahmadaldda04@gmail.com>
2024-12-29 15:58:20 +0200

    first commit

--- eBook Library Service/obj/Debug/net8.0/eBook Library Service.AssemblyInfo.cs
+++ eBook Library Service/obj/Debug/net8.0/eBook Library Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eBook Library Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103d645e3b4f8a41d57640feefe5b1c398ecad42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e524d53580355b2791048b38ad145e3aaedde4")]
 [assembly: System.Reflection.AssemblyProductAttribute("eBook Library Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eBook Library Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
